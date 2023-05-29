284206f11af8fb5adccf03d2cb5035bdbb6989ed
Iron-E <code.iron.e@gmail.com>
2023-05-29 16:02:29 -0400

    fix(Adapter): unresolved `Db`

--- src/schema/adapter.rs
+++ src/schema/adapter.rs
@@ -1,7 +1,3 @@
-use sqlx::Database;
-
-use crate::Deletable;
-
 use super::{
 	ContactAdapter,
 	EmployeeAdapter,
@@ -11,31 +7,29 @@ use super::{
 	OrganizationAdapter,
 	TimesheetAdapter,
 };
+use crate::{Deletable, Initializable};
 
 /// A complete implementation of the [`winvoice_adapter::schema`].
-pub trait Adapter
+pub trait Adapter: Initializable
 {
-	/// The [`Database`] that is used for this adapter.
-	type Db: Database;
-
 	/// The adapter for [`Contact`](winvoice_schema::Contact)s
-	type Contact: Deletable<Db = Db> + ContactAdapter;
+	type Contact: Deletable<Db = Self::Db> + ContactAdapter;
 
 	/// The adapter for [`Employee`](winvoice_schema::Employee)s
-	type Employee: Deletable<Db = Db> + EmployeeAdapter;
+	type Employee: Deletable<Db = Self::Db> + EmployeeAdapter;
 
 	/// The adapter for [`Expense`](winvoice_schema::Expense)s
-	type Expenses: Deletable<Db = Db> + ExpensesAdapter;
+	type Expenses: Deletable<Db = Self::Db> + ExpensesAdapter;
 
 	/// The adapter for [`Job`](winvoice_schema::Job)s
-	type Job: Deletable<Db = Db> + JobAdapter;
+	type Job: Deletable<Db = Self::Db> + JobAdapter;
 
 	/// The adapter for [`Location`](winvoice_schema::Location)s
-	type Location: Deletable<Db = Db> + LocationAdapter;
+	type Location: Deletable<Db = Self::Db> + LocationAdapter;
 
 	/// The adapter for [`Organization`](winvoice_schema::Organization)s
-	type Organization: Deletable<Db = Db> + OrganizationAdapter;
+	type Organization: Deletable<Db = Self::Db> + OrganizationAdapter;
 
 	/// The adapter for [`Timesheet`](winvoice_schema::Timesheet)s
-	type Timesheet: Deletable<Db = Db> + TimesheetAdapter;
+	type Timesheet: Deletable<Db = Self::Db> + TimesheetAdapter;
 }
